593524f3afb57a8188f9d4275220779836863c50
JohnnyLuis <stirpe.1752713@studenti.uniroma1.it>
2019-06-22 14:53:17 +0200

    sem_open version 1 in disastrOS_semopen.c; added Semaphore_init and SemDescriptor_init in disastrOS.c; added semaphores_list in disastrOS_globals.h

--- disastrOS_semopen.c
+++ disastrOS_semopen.c
@@ -7,5 +7,22 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semOpen(){
-  // do stuff :)
+  int id = running->syscall_args[0];
+  int count = 1;
+
+  Semaphore* res=SemaphoreList_byId(&semaphores_list, id);
+  if (!res) {
+	res = Semaphore_alloc(id,count);
+	List_insert(&semaphores_list,semaphores_list.last,(ListItem*)res);
+  }
+
+  SemDescriptor* des=SemDescriptor_alloc(running->last_fd, res, running);
+  running->last_fd++;
+  SemDescriptorPtr* desptr=SemDescriptorPtr_alloc(des);
+  List_insert(&running->descriptors, running->descriptors.last, (ListItem*) des);
+
+  des->ptr=desptr;
+  List_insert(&res->descriptors, res->descriptors.last, (ListItem*) desptr);
+
+  running->syscall_retvalue = des->fd;
 }
