76d315f68a5c554460f2ce2819a8e821bcc9357b
JohnnyLuis <stirpe.1752713@studenti.uniroma1.it>
2019-06-29 11:08:08 +0200

    added comments and new function to test MAX_NUM_SEMDESCRIPTORS_PER_PROCESS

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -17,27 +17,59 @@ void sleeperFunction(void* args){
   }
 }
 
+void testMaxNumSemdescriptorsPerProcessFunction(void* args) {
+  int sem_count = 1;
+  int mode = DSOS_CREATE;
+  int sem_fd_array[MAX_NUM_SEMDESCRIPTORS_PER_PROCESS + 1];
+  
+  printf("last semopen should return an error\n");
+  for (int i=0 ; i <= MAX_NUM_SEMDESCRIPTORS_PER_PROCESS ; i++) {
+    sem_fd_array[i] = disastrOS_semOpen(i+15,sem_count,mode);
+  }
+  
+  disastrOS_printStatus();
+  
+  for (int i=0 ; i <= MAX_NUM_SEMDESCRIPTORS_PER_PROCESS ; i++) {
+    disastrOS_semClose(sem_fd_array[i]);
+  }
+  
+  disastrOS_printStatus();
+  
+  disastrOS_exit(disastrOS_getpid()+1);
+}
+
 void childFunction(void* args){
   printf("Hello, I am the child function %d\n",disastrOS_getpid());
   printf("I will iterate a bit, before terminating\n");
   int sem_fd;
   int type=0;
   int mode=0;
+  int sem_count = 1;
   int fd=disastrOS_openResource(disastrOS_getpid(),type,mode);
   printf("fd=%d\n", fd);
   
+  //child processes with odd pid will try to create semaphores that already exist ...
   if (disastrOS_getpid() % 2) {
     mode = DSOS_CREATE;
-    sem_fd = disastrOS_semOpen(disastrOS_getpid(),1,mode);
+    sem_fd = disastrOS_semOpen(disastrOS_getpid(),sem_count,mode);
+    
+    //... moreover, child #5 and child #7 will open semaphore #6 and then they will execute a semwait and will be blocked between
+    //semwait and sempost instructions: in this way, the next process who executes semwait on sem #6 will be put into the waiting queue
+    //of the semaphore...
     if (disastrOS_getpid() == 5 || disastrOS_getpid() == 7) {
-      sem_fd = disastrOS_semOpen(6,1,0);
+      mode = 0;
+      
+      sem_fd = disastrOS_semOpen(6,sem_count,mode);
       disastrOS_semWait(sem_fd);
       disastrOS_sleep(4);
       disastrOS_semPost(sem_fd);
+    
     }
   }
+  
+  //child processes with even pid will open semaphores that's been already created. then they will execute a semwait and a sempost
   else {
-    sem_fd = disastrOS_semOpen(disastrOS_getpid(),1,mode);
+    sem_fd = disastrOS_semOpen(disastrOS_getpid(),sem_count,mode);
     disastrOS_semWait(sem_fd);
     disastrOS_semPost(sem_fd);
   }
@@ -45,11 +77,11 @@ void childFunction(void* args){
   disastrOS_semClose(sem_fd);
   
   printf("PID: %d, terminating\n", disastrOS_getpid());
-
+  /*
   for (int i=0; i<(disastrOS_getpid()+1); ++i){
     printf("PID: %d, iterate %d\n", disastrOS_getpid(), i);
     disastrOS_sleep((20-disastrOS_getpid())*5);
-  }
+  }*/
   disastrOS_exit(disastrOS_getpid()+1);
 }
 
@@ -58,29 +90,35 @@ void initFunction(void* args) {
   disastrOS_printStatus();
   printf("hello, I am init and I just started\n");
   disastrOS_spawn(sleeperFunction, 0);
-  
+  int type = 0;
+  int mode=DSOS_CREATE;
+  int sem_count = 1;
 
   printf("I feel like to spawn 10 nice threads\n");
   int alive_children=0;
   for (int i=0; i<10; ++i) {
-    int type=0;
-    int mode=DSOS_CREATE;
     printf("mode: %d\n", mode);
     printf("opening resource (and creating if necessary)\n");
     int fd=disastrOS_openResource(i,type,mode);
     printf("fd=%d\n", fd);
 	
+    //creating new semaphores...
 	  printf("opening semaphore (and creating if necessary)\n");
-	  int sem_fd=disastrOS_semOpen(i,1,mode);
+	  int sem_fd=disastrOS_semOpen(i,sem_count,mode);
     printf("sem_fd=%d\n\n", sem_fd);
 	
     disastrOS_spawn(childFunction, 0);
     alive_children++;
   }
   
+  //I try to open a semaphore that it's never been created before
+  mode = 0;
   printf("opening semaphore\n");
-	int sem_fd=disastrOS_semOpen(50,1,0);
+	int sem_fd=disastrOS_semOpen(50,sem_count,mode);
   printf("sem_fd=%d\n\n", sem_fd);
+  
+  disastrOS_spawn(testMaxNumSemdescriptorsPerProcessFunction, 0);
+  alive_children++;
 
   disastrOS_printStatus();
   int retval;
@@ -91,6 +129,7 @@ void initFunction(void* args) {
 	   pid, retval, alive_children);
     --alive_children;
   }
+  disastrOS_printStatus();
   printf("shutdown!");
   disastrOS_shutdown();
 }
